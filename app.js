4af0a5ff946e21652044976b2483432b2e85134c
victor-chukwu <okenwa1993@gmail.com>
2021-01-29 00:25:36 +0100

    added delete and check btn

--- app.js
+++ app.js
@@ -1,81 +1,80 @@
+//selectors
+const todoInput = document.querySelector('.todoPablo');
+const todoButton = document.querySelector('.adbtn');
+const todoList = document.querySelector('.todo-list');
 
-// write a function to add todo
-let AddTodo = (value) =>{
-    //retrieve ul
-    let ul = document.querySelector('ul');
+//event listeners
+todoButton.addEventListener("click", addTodo)
+todoList.addEventListener("click", deleteCheck)
+document.getElementById('clearall').addEventListener('click', handleClearAll);
+//functions
 
-    // create a list element
-    let li = document.createElement('li');
+function addTodo(event) {
+    event.preventDefault();
+    //todo DIV
+    console.log(event)
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    //todo LI 
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo_item');
+    todoDiv.appendChild(newTodo);
+    if(todoInput.value === ""){
+        return null
+    }
+    //check mark BUTTON
+    const completedButton = document.createElement('button');
+    completedButton.classList.add('complete_btn');
+    completedButton.innerHTML = "<i  class='fas fa-check-square'></i>";
+    todoDiv.appendChild(completedButton);
+    console.log(todoDiv)
+    //delete BUTTON
+    const deleteButton = document.createElement('button');
+    deleteButton.innerHTML = "<i class='fas fa-trash'></i>";
+    deleteButton.classList.add('delete_btn');
+    todoDiv.appendChild(deleteButton);
+    //Append to Actual LIST
+    todoList.appendChild(todoDiv);
+    console.log(todoList);
 
-    //to create child elements for li
-    li.innerHTML = `<span class='todoitem'>${value}</span>
-    <button class='checked'><i class='fas fa-check-square'></i></button>
-    <button class='delete'><i class='fas fa-trash'></i></button>`;
+    //Clear todo input VALUE
+    todoInput.value = ""
+}
+// console.log(addTodo());
 
-    //add class to d li element
-    li.classList.add('todo-list-item');
-
-    ul.appendChild(li);
+//DELETE & CHECK
+function deleteCheck(event) {
+    const item = event.target.parentElement;
+    console.log(event);
+    //DELETE ITEM
+    if (item.classList[0] === "delete_btn") {
+        let todo = item.parentElement;
+        console.log(todo);
+        //ANIMATION TRANSITION
+        todo.classList.add("fall")
+        todo.addEventListener('transitionend', function () {
+            todo.remove()
+        })
+    }
+    //COMPLETE ITEM
+    if (item.classList[0] === "complete_btn") {
+        console.log(item.classList[0])
+        const todo = item.parentElement;
+        console.log(todo)
+        todo.classList.toggle("completedItem")
+    }
 }
 
-// event listener function
-let formSubmit = (e) =>{
-    //to avoid browser to not submit by default behaviour
-    e.preventDefault();
-    //retrieve input from text input field
-
-    let input = document.querySelector('input')
-    // check if there is a value in an input field and add the value to todo list
-    if(input.value != ''){
-        AddTodo(input.value);
-        input.value = '';
-    }
-
-};
-document.querySelector('form').addEventListener('submit', formSubmit);
-
-
-//write the checke function
-function checke(e) {
-    //im stuck here
-    let item = e.target.parentNode;
+function handleClearAll(event) {
+    document.querySelector('.todo-list').innerHTML = '';
     
-    if(item.style.textDecoration == 'line-through'){
-        item.style.textDecoration = 'none';
-    }else {
-        item.style.textDecoration = 'line-through';
-    }
-};
-// console.log(checke())
-let deleted = (e) =>{
-
 }
 
-
-//write a delete or check function to handle the click event
-let checkedOrDelete = (e) =>{
-    //since the buttons have names we checke through their names
-    if(e.target.name === 'checked'){
-        //pass the check function
-        checke();
-    }
-
-    if(e.target.name === 'delete'){
-        //pass a delete function
-        deleted();
-    }
-    // console.log(target.name)
-
-};
+let checkbox = document.getElementById('checkbox');
+checkbox.addEventListener('change',  () => {
+    document.body.classList.toggle('white');
+});
 
 
-
-
-
-
-//add an event listener to check if its delete or check button
-document.querySelector('ul').addEventListener('click', checkedOrDelete);
-
-
-
-
+console.log(checkbox)
